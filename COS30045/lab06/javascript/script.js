56d03382d3e399c838c94b76e2edb8383d604d69
luannn010 <103078945+luannn010@users.noreply.github.com>
2023-04-11 01:38:51 +1000

    Push

--- COS30045/lab06/javascript/script.js
+++ COS30045/lab06/javascript/script.js
@@ -1,5 +1,5 @@
 var w = 900;
-var h = 300; 
+var h = 500; 
 var barPadding = 1;
 var dataset =[14, 5, 26, 23 , 9, 15, 21, 8, 7, 16, 30, 28, 26, 15, 12, 10, 16, 19];
 var svg1;
@@ -19,53 +19,85 @@ function BarChart(dataset,delay,duration,ease) {
                  .data(dataset)
 
   
-  bars.enter()
-      .append("rect")
-      .merge(bars)
-      .transition()
-      .delay(delay)
-      .duration(duration)
-      .ease(ease)
-      .attr("x", (d,i) => xScale(i))
-      .attr("y", d =>  yScale(d))
-      .attr("width", xScale.bandwidth())
-      .attr("height", d => h - yScale(d))
-      .style("fill", d =>"rgb(1, " + d*10 + ",10)");
-  bars.exit()
-      .transition()
-      .delay(delay)
-      .duration(duration)
-      .attr("x", w)
-      .ease(ease)
-      .remove();
+bars.enter()
+    .append("rect")
+    .style("fill", d => "rgb(1, " + d * 10 + ",10)")
+    .on("mouseover", function(event, d) {
+      //change color
+      d3.select(this)
+         .style("fill", "orange");
+
+      //Add tooltip
+
+      var xPosition = parseFloat(d3.select(this).attr("x")) +25
+      var yPosition = parseFloat(d3.select(this).attr("y")) +15
+
+      svg1.append("text")
+          .attr("id","tooltip")
+          .attr("x", xPosition)
+          .attr("y", yPosition)
+          .text(d)
+          .attr("font-family", "sans-serif")
+          .attr("font-size", "12px")
+          .attr("fill", "black")
+          .attr("text-anchor", "middle")
+          .style("font-weight", "bold");
+        })
+      
+    .on("mouseout", function() {
+      //Return color
+      d3.select(this)
+         .style("fill", d =>"rgb(1, " + d*10 + ",10)");
+      //Remove tooltip
+      d3.select("#tooltip").remove();
+        })
+    .merge(bars)
+    .transition()
+    .delay(delay)
+    .duration(duration)
+    .ease(ease)
+    .attr("x", (d,i) => xScale(i))
+    .attr("y", d => yScale(d))
+    .attr("width", xScale.bandwidth())
+    .attr("height", d => h - yScale(d))
+
+    
+bars.exit()
+    .transition()
+    .delay(delay)
+    .duration(duration)
+    .attr("x", w)
+    .ease(ease)
+    .remove();
 
   // Label
   // Select text elements and bind data to them
-var labels = svg1.selectAll("text")
-.data(dataset);
+// var labels = svg1.selectAll("text")
+//                  .data(dataset);
 
-labels.enter()
-  .append("text")
-  .merge(labels)
-  .transition() 
-  .delay(delay)
-  .duration(duration)
-  .ease(ease)
-  .text(d => d)
-  .attr("x", (d, i) => xScale(i) + xScale.bandwidth() / 2)
-  .attr("y", d => yScale(d) + 10)
-  .attr("font-family", "sans-serif")
-  .attr("font-size", "11px")
-  .attr("fill", "white")
-  .attr("text-anchor", "middle");
+// labels.enter()
+//       .append("text")
+//       .merge(labels)
+//       .transition() 
+//       .delay(delay)
+//       .duration(duration)
+//       .ease(ease)
+//       .text(d => d)
+//       .attr("x", (d, i) => xScale(i) + xScale.bandwidth() / 2)
+//       .attr("y", d => yScale(d) + 10)
+//       .attr("font-family", "sans-serif")
+//       .attr("font-size", "11px")
+//       .attr("fill", "white")
+//       .attr("text-anchor", "middle");
 
-// Remove old labels
-labels.exit()
-.transition()
-.delay(0)
-.duration(duration)
-.ease(ease)
-.remove();
+
+// // Remove old labels
+// labels.exit()
+//       .transition()
+//       .delay(0)
+//       .duration(duration)
+//       .ease(ease)
+//       .remove();
 
 }
 //Lab5.1
@@ -148,6 +180,32 @@ function RemoveBars() {
      
   })
 }
+//6.2
+//Ascending
+function Ascending() {
+  d3.select("#asc")
+    .on("click", () => {
+      svg1.selectAll("rect")
+          .sort((a, b) =>  {
+            return d3.ascending(a, b);
+          })
+          .attr("x", (d,i) => xScale(i));
+    
+    })
+}
+//Descending
+function Descending() {
+  d3.select("#desc")
+    .on("click", () => {
+      svg1.selectAll("rect")
+          .sort((a, b) =>  {
+            return d3.descending(a, b);
+          })
+          .attr("x", (d,i) => xScale(i));
+    
+    })
+}
+
 function main(){
   svg1 = d3.select("#chart1")
            .append("svg")
@@ -159,6 +217,8 @@ function main(){
   Transition2();
   AddBars();
   RemoveBars();
+  Ascending();
+  Descending();
 }
 
 window.onload = function() {
